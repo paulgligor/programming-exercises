2ac195b91cd07d84ebfc64318e450ebd1f253b38
paulgligor <paul.gligor@gmail.com>
2018-12-12 15:00:49 +0000

    max counter exercise desc

---
+++ Solution.Tests/Level 4 - Counting Elements/MaxCounterTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace Solution.Tests
+{
+    public class MaxCounterTests
+    {
+        [Fact]
+        public void Method_InitialCondition_ExpectedResult()
+        {
+            //Arrange - Given
+
+
+            //Act - When
+
+
+            //Assert - Then
+
+        }
+    }
+}
