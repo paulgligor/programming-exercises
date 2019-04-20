f77177880de3ce4381dacb26a5e46e417c434995
paulgligor <paul.gligor@gmail.com>
2019-04-20 12:03:37 +0100

    PassingCars lesson

---
+++ Solution.Tests/Level 5 - Prefix Sums/PassingCarsTests.cs
@@ -0,0 +1,25 @@
+﻿using Solution.Service.Level_5___Prefix_Sums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Solution.Tests
+{
+    public class PassingCarsTests
+    {
+        [Fact]
+        public void Solution_SmallArray_Correct()
+        {
+            //Arrange - Given
+            var array = new int[] { 0, 1, 0, 1, 1 };
+
+            //Act - When
+            var result = PassingCars.Solution(array);
+
+            //Assert - Then
+            var expectedResult = 5;
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
