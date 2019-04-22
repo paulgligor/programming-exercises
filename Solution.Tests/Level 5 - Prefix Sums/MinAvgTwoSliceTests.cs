941328edb56de403810194301a01eb9434e99da5
paulgligor <paul.gligor@gmail.com>
2019-04-22 15:19:52 +0100

    Started MinAvgTwoSlice

---
+++ Solution.Tests/Level 5 - Prefix Sums/MinAvgTwoSliceTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Solution.Tests.Level_5___Prefix_Sums
+{
+    public class MinAvgTwoSliceTests
+    {
+        [Fact]
+        public void Solution_SmallArray_Correct()
+        {
+            //Arrange - Given
+
+            //Act - When
+
+            //Assert - Then
+
+        }
+    }
+}
