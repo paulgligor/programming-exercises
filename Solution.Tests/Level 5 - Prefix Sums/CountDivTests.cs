d62af32ec6b4b2d530009a5610a964720418a942
paulgligor <paul.gligor@gmail.com>
2019-04-23 20:07:44 +0100

    Create CountDiv

---
+++ Solution.Tests/Level 5 - Prefix Sums/CountDivTests.cs
@@ -0,0 +1,28 @@
+﻿using Solution.Service.Level_5___Prefix_Sums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Solution.Tests.Level_5___Prefix_Sums
+{
+    public class CountDivTests
+    {
+        [Fact]
+        public void Solution_SmallNumbers_Correct()
+        {
+            //Arrange - Given
+            var startNumer = 5;
+            var endNumber = 11;
+            var dividedBy = 2;
+
+            //Act - When
+            var result = CountDiv.Solution(startNumer, endNumber, dividedBy);
+
+            //Assert - Then
+            var expectedResult = 3;
+            Assert.Equal(expectedResult, result);
+
+        }
+    }
+}
