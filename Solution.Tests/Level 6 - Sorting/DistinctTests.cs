1d19cae4cbc170cccf8e94ca33a01da6b8702fcb
paulgligor <paul.gligor@gmail.com>
2019-04-23 20:11:51 +0100

    Start Distinct

---
+++ Solution.Tests/Level 6 - Sorting/DistinctTests.cs
@@ -0,0 +1,22 @@
+﻿using Solution.Service.Level_6___Sorting;
+using Xunit;
+
+namespace Solution.Tests
+{
+    public class DistinctTests
+    {
+        [Fact]
+        public void Solution_SmallArray_Correct()
+        {
+            //Arrange - Given
+            var array = new int[] { 2, 1, 1, 2, 3, 1 };
+
+            //Act - When
+            var result = Distinct.Solution(array);
+
+            //Assert - Then
+            var expectedResult = 5;
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
