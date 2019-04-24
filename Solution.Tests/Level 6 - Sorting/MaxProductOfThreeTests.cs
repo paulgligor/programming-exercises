fd5bf080e8bcf1e71ffe1b70e45878619bc487af
paulgligor <paul.gligor@gmail.com>
2019-04-24 13:47:38 +0100

    Start MaxProductOfThree

---
+++ Solution.Tests/Level 6 - Sorting/MaxProductOfThreeTests.cs
@@ -0,0 +1,22 @@
+﻿using Solution.Service.Level_6___Sorting;
+using Xunit;
+
+namespace Solution.Tests.Level_6___Sorting
+{
+    public class MaxProductOfThreeTests
+    {
+        [Fact]
+        public void Solution_SmallArray_Correct()
+        {
+            //Arrange - Given
+            var array = new int[] { -3, 1, 2, -2, 5, 6 };
+
+            //Act - When
+            var result = MaxProductOfThree.Solution(array);
+
+            //Assert - Then
+            var expectedResult = 60;
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
