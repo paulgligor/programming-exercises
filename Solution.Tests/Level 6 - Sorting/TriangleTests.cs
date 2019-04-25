68cf094fce0137d1e80a8598679eeb27c4247fab
paulgligor <paul.gligor@gmail.com>
2019-04-25 11:20:53 +0100

    Create Triangle

---
+++ Solution.Tests/Level 6 - Sorting/TriangleTests.cs
@@ -0,0 +1,25 @@
+﻿using Solution.Service.Level_6___Sorting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Solution.Tests.Level_6___Sorting
+{
+    public class TriangleTests
+    {
+        [Fact]
+        public void Solution_SmallArray_Correct()
+        {
+            //Arrange - Given
+            var array = new int[] { 10, 2, 5, 1, 8, 20 };
+
+            //Act - When
+            var result = Triangle.Solution(array);
+
+            //Assert - Then
+            var expectedResult = 1;
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
