d17a39bbb9218f28f060309dadb68d482e571b08
paulgligor <paul.gligor@gmail.com>
2019-04-26 12:03:57 +0100

    Created NumberOfDiscIntersections

---
+++ Solution.Tests/Level 6 - Sorting/NumberOfDiscIntersectionsTests.cs
@@ -0,0 +1,25 @@
+﻿using Solution.Service.Level_6___Sorting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Solution.Tests.Level_6___Sorting
+{
+    public class NumberOfDiscIntersectionsTests
+    {
+        [Fact]
+        public void Solution_SmallArray_Correct()
+        {
+            //Arrange - Given
+            var array = new int[] { 1, 5, 2, 1, 4, 0 };
+
+            //Act - When
+            var result = NumberOfDiscIntersections.Solution(array);
+
+            //Assert - Then
+            var expectedResult = 11;
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
