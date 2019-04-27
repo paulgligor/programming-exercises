269346d87bd40653c74363b03a868aa879b5c2cb
paulgligor <paul.gligor@gmail.com>
2019-04-27 11:22:10 +0100

    Start Level 7, Brackets

---
+++ Solution.Tests/Level 7 - Stacks and Queues/BracketsTests.cs
@@ -0,0 +1,68 @@
+﻿using Solution.Service.Level_7___Stacks_and_Queues;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Solution.Tests.Level_7___Stacks_and_Queues
+{
+    public class BracketsTests
+    {
+        [Fact]
+        public void Solution_EmptyString_Correct()
+        {
+            //Arrange - Given
+            var aString = "";
+
+            //Act - When
+            var result = Brackets.Solution(aString);
+
+            //Assert - Then
+            var expectedResult = 1;
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void Solution_SmallString_Correct()
+        {
+            //Arrange - Given
+            var aString = "{[()()]}";
+
+            //Act - When
+            var result = Brackets.Solution(aString);
+
+            //Assert - Then
+            var expectedResult = 1;
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void Solution_SmallString2_Correct()
+        {
+            //Arrange - Given
+            var aString = "(U)";
+
+            //Act - When
+            var result = Brackets.Solution(aString);
+
+            //Assert - Then
+            var expectedResult = 1;
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void Solution_SmallString_Incorrect()
+        {
+            //Arrange - Given
+            var aString = "([)()]";
+
+            //Act - When
+            var result = Brackets.Solution(aString);
+
+            //Assert - Then
+            var expectedResult = 1;
+            Assert.Equal(expectedResult, result);
+        }
+
+    }
+}
