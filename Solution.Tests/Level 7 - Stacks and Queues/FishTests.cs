097440494bd4988c5fffa43f2838e1d16f05b229
paulgligor <paul.gligor@gmail.com>
2019-04-29 23:46:47 +0100

    Start Fish Solution

---
+++ Solution.Tests/Level 7 - Stacks and Queues/FishTests.cs
@@ -0,0 +1,26 @@
+﻿using Solution.Service.Level_7___Stacks_and_Queues;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Solution.Tests.Level_7___Stacks_and_Queues
+{
+    public class FishTests
+    {
+        [Fact]
+        public void Solution_SmallString3_Correct()
+        {
+            //Arrange - Given
+            var A = new int[] { 4, 3, 2, 1, 5 };
+            var B = new int[] { 0, 1, 0, 0, 0 };
+            
+            //Act - When
+            var result = Fish.Solution(A, B);
+
+            //Assert - Then
+            var expectedResult = -1;
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
